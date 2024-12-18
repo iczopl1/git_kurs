8b07b3df6859f06a3750843d03a19249c4341304
Ireneusz Przystaś <irrk@op.pl>
2024-12-18 19:08:00 +0100

    test

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def main():
     print("Elo")
 if __name__ == "__main__":
     main()
-    # //zmianElElEloooa
     # //Witamy wszystkich zgromadzonych przed komputerami xD :)
     # //zmiana elo dasdasd asf af ds
     # //Witamy wszystkich zgromadzonych przed komputerami xD :)
